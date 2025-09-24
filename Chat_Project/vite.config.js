978953182130baa63ac09b269cd7692d99acd7e7
Abdul Mecit Usda <92247286+mecitusda@users.noreply.github.com>
2025-09-25 02:09:14 +0300

    redirects fix 2

--- Chat_Project/vite.config.js
+++ Chat_Project/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // dev server için zaten var
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
   },
-})
+  publicDir: "public", // 👈 buradan kopyalanacak
+});
